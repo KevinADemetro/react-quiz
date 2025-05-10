0714fa597efb4c79a4b03eea44bbfb5e9ed086d8
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-10 09:19:44 -0300

    usando reducer para subistituir o state count

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -4,7 +4,9 @@ function DateCounter() {
   // const [count, setCount] = useState(0);
   function reducer(state, action) {
     console.log(state, action);
-    return state + action;
+    if (action.type === "inc") return state + 1;
+    if (action.type === "dec") return state - 1;
+    if (action.type === "setCount") return action.payload;
   }
   const [count, dispatch] = useReducer(reducer, 0);
 
@@ -15,19 +17,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: "inc" });
 
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
